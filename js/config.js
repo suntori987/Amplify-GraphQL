e4e842c8dc1703ce05241942ca9d02b543d8fe3a
sunrise999 <sunhexudong@126.com>
2026-06-25 15:37:17 +0900

    Update Cognito configuration in config.js
    Updated Cognito configuration with new user pool details.

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'ap-northeast-1_I4MXOuCa1', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3a6rc4iqasfesuig1oark9nh7s', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-northeast-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' //本次不使用，这里请留空
